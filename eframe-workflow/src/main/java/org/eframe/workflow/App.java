64e3c63f98e3258b122ff33f108669d33608b777
EE <emmerich@126.com>
2016-04-25 14:23:55 +0800

    工作流

---
+++ eframe-workflow/src/main/java/org/eframe/workflow/App.java
@@ -0,0 +1,13 @@
+package org.eframe.workflow;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
