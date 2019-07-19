f1a6c049e0531afe7362edfa84de2e2e399d50bd
liangruilin <liangruilin@yy.com>
2019-07-19 15:07:18 +0800

    缓存测试

---
+++ eframe-sb/src/test/java/cache/LockTest.java
@@ -0,0 +1,22 @@
+package cache;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.Application;
+
+import db.DBBaseTest;
+
+/**
+ * redis排它锁测试
+ * @Date 2019年7月19日
+ * @author E.E.
+ *
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
+public class LockTest extends DBBaseTest{
+
+	
+}
--- eframe-sb/src/test/java/init/InitRole.java
+++ eframe-sb/src/test/java/init/InitRole.java
@@ -1,11 +1,17 @@
 package init;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import com.Application;
 import com.alibaba.druid.pool.DruidDataSource;
 
 import db.DBBaseTest;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
 public class InitRole extends DBBaseTest{
 	
 	//生成全部功能用户组
